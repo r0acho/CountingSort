296a8b61941240e154db211e0f32c1a449c96004
sweetie <ivan.linzov@gmail.com>
2022-08-10 15:35:07 +0300

    added counting sort working with digits

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿//сортировка подсчетом
+
+int[] array = {3, 2, 1, 5, 9};
+
+CountingSort(array);
+
+Console.WriteLine(string.Join(", ", array));
+
+void CountingSort(int[] inputArray)
+{
+    int[] counters = new int[10]; //массив повторений
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        counters[inputArray[i]]++;
+        // ourNumber = inputArray[i];
+        // counters[ourNumber]++;
+    }
+
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            inputArray[index] = i;
+            index++;
+        }
+    }
+}
+
