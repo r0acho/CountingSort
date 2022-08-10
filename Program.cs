5c24b0c30bbab180988b0b3f95d6962a14d99ffa
sweetie <ivan.linzov@gmail.com>
2022-08-10 16:04:10 +0300

    added extended counting sort

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿//сортировка подсчетом
 
-int[] array = {3, 2, 1, 5, 9};
+int[] array = {-10, -5, -9, 0, 2, 5, 1, 3, 1, 0, 1};
+int[] sortedArray = CountingSortExtended(array);
 
-CountingSort(array);
+//CountingSort(array);
 
-Console.WriteLine(string.Join(", ", array));
+Console.WriteLine(string.Join(", ", sortedArray));
 
 void CountingSort(int[] inputArray)
 {
@@ -28,3 +29,32 @@ void CountingSort(int[] inputArray)
     }
 }
 
+
+int[] CountingSortExtended(int[] inputArray)
+{
+    int max = inputArray.Max();
+    int min = inputArray.Min();
+
+    int offset = -min;
+    int[] sortedArray = new int[inputArray.Length];
+    int[] counters = new int[max + offset + 1];
+
+
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        counters[inputArray[i] + offset]++;
+    }
+
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            sortedArray[index] = i - offset;
+            index++;
+        }
+    }
+
+    return sortedArray;
+}
